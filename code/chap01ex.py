60608b3196da16654c09566f1d5bb10eacaa7606
lpoliakova <olena.poliakova@gmail.com>
2018-07-12 11:54:51 +0300

    Chapter 1 exercised finished

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -5,22 +5,82 @@ Copyright 2014 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
-from __future__ import print_function
-
-import numpy as np
-import sys
-
 import nsfg
 import thinkstats2
 
+from collections import defaultdict
 
-def main(script):
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    """Reads the NSFG respondent data.
+    dct_file: string file name
+    dat_file: string file name
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+
+def CleanFemResp(df):
+    """Recodes variables from the respondent frame.
+    df: DataFrame
+    """
+    pass
+
+
+def ValidatePregnum(resp):
+    """Validate pregnum in the respondent file.
+    resp: respondent DataFrame
+    preg: pregnancy DataFrame
+    """
+
+    preg = nsfg.ReadFemPreg()
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = MakePregMap(preg)
+    
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
+
+
+def MakePregMap(df):
+    """Make a map from caseid to list of preg indices.
+    df: DataFrame
+    returns: dict that maps from caseid to list of indices into `preg`
+    """
+    d = defaultdict(list)
+    for index, caseid in df.caseid.iteritems():
+        d[caseid].append(index)
+    return d
+
+
+def main():
     """Tests the functions in this module.
-
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    # read and validate the respondent file
+    resp = ReadFemResp()
+
+    assert(len(resp) == 7643)
+    assert(resp.pregnum.value_counts()[1] == 1267)
+    assert(ValidatePregnum(resp))
+
+    print('All tests passed.')
 
 
 if __name__ == '__main__':
-    main(*sys.argv)
+    main()
